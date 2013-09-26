d7bb3a74d17c421378674ed121186c214f83c161
Jimmy Liang <lyh@yazotech.com>
2013-09-27 00:31:34 +0800

    err

--- app/controllers/travel_admin/input_types_controller.rb
+++ app/controllers/travel_admin/input_types_controller.rb
@@ -1,8 +1,8 @@
 module TravelAdmin
 	class InputTypesController < ResourceController
+    private
+      def input_type_params
+        params.require(:input_type).permit(:type_name, :type_text, :type_value, :status)
+      end
 	end
-  private
-    def input_type_params
-      params.require(:input_type).permit(:type_name, :type_text, :type_value, :status)
-
 end
